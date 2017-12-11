b7252927d8d957c55a8a55f920a82942af2c3dd4
zhong clock <zhong.clock@gmail.com>
2017-12-11 18:59:32 +0800

    getIPFromIP138() works now

---
+++ Tests/testUtilities.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+import os
+import sys
+sys.path.append('..')
+import Utilities
+import unittest
+
+class testUtilities(unittest.TestCase):
+    def testGetURL(self):
+        url = "http://www.baidu.com"
+        htmlCont=Utilities.getURL(url)
+        #print htmlCont
+        #Baidu's webpage content should be bigger than 100 bytes
+        self.assertGreater(len(htmlCont), 100)
+
+        url = Utilities.IPReportWebList[0]
+        htmlCont = Utilities.getURL(url)
+        self.assertGreater(len(htmlCont), 100)
+        #print htmlCont
+
+        url = Utilities.IPReportWebList[1]
+        htmlCont = Utilities.getURL(url)
+        self.assertGreater(len(htmlCont), 100)
+        #print htmlCont
+
+    def testGetIP(self):
+        ipAddr=Utilities.getIPFromIP138()
+        print ipAddr
+if __name__ == '__main__':
+	unittest.main()
--- Utilities.py
+++ Utilities.py
@@ -1,3 +1,33 @@
 #! /usr/bin/env python
 
+import urllib2
 
+
+HTTP_TIMEOUT=38
+AgentList=['Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11',
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0"]
+
+#it seems 'http://www.iplocation.net/' couldn't work
+IPReportWebList=['http://www.infosniper.net/', 'http://2017.ip138.com/ic.asp','http://www.iplocation.net/']
+
+
+def getURL(url):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', AgentList[1])]
+    text = ""
+    try:
+        openerConnection = opener.open(url, timeout=HTTP_TIMEOUT)
+        text = openerConnection.read()
+    except Exception as e:
+        print e
+        return text
+    return text
+
+def getIPFromIP138():
+    url = IPReportWebList[1]
+    htmlCont= getURL(url)
+    startChar = '['
+    endChar   = ']'
+    startIndex = htmlCont.index(startChar)+len(startChar)
+    endIndex   = htmlCont.index(endChar)
+    return htmlCont[startIndex:endIndex]
