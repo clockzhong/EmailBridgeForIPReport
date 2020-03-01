b9cca07a3d796256f0f0244007ebdd53990382c9
zhong clock <zhong.clock@gmail.com>
2020-03-01 22:44:06 +0800

    added notifyIP2.py

---
+++ emailDNS/notifyIP2.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import sys
+import urllib2
+from email.mime.text import MIMEText
+import smtplib
+import time
+import Utilities
+import random
+import platform
+
+if len(sys.argv)!=4 :
+    print "notifyIP.py YouEmailAddress YouPasswordForYourEmailAccount receiverEmailAddr"
+    sys.exit(1)
+
+myEmailAddr = sys.argv[1]
+password = sys.argv[2]
+otherEmailAddr = sys.argv[3]
+
+print(myEmailAddr, password, otherEmailAddr)
+
+MinimumPauseTime = 50
+MaximumPauseTime = 60*15
+
+MinimumPauseTime = 50/25
+MaximumPauseTime = 60*15/60
+
+hostName = platform.node()
+msg = " from "+hostName
+print msg
+#quit()
+
+while True:
+    print "check the IP"
+    Utilities.notifyIP2(myEmailAddr, otherEmailAddr, password, msg)
+    pauseTime = random.randint(MinimumPauseTime, MaximumPauseTime)
+    print "wait for ",pauseTime, " seconds"
+    time.sleep(pauseTime)
